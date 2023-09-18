c2ee4abc7ca305f12a12befe6d6d1163bb3fa754
ninajavier <ninajavier13@gmail.com>
2023-09-18 15:46:25 -0400

    Attempting to siable all broken firebase code for deployment

--- config/firebaseAdminConfig.js
+++ config/firebaseAdminConfig.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const admin = require('firebase-admin');
+// require('dotenv').config();
+// const admin = require('firebase-admin');
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    clientId: process.env.FIREBASE_CLIENT_ID,
-    authUri: "https://accounts.google.com/o/oauth2/auth",
-    tokenUri: "https://oauth2.googleapis.com/token",
-    authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
-    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
-  }),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+//     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//     clientId: process.env.FIREBASE_CLIENT_ID,
+//     authUri: "https://accounts.google.com/o/oauth2/auth",
+//     tokenUri: "https://oauth2.googleapis.com/token",
+//     authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+//     clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
+//   }),
+// });
 
-module.exports = admin;
+// module.exports = admin;
