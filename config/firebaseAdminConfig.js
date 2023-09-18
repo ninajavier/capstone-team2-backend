6c07660d9c4e715a66e1f0ee83fcf623ceb8220c
ninajavier <ninajavier13@gmail.com>
2023-09-17 23:56:54 -0400

    trying again2

--- config/firebaseAdminConfig.js
+++ config/firebaseAdminConfig.js
@@ -5,7 +5,9 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n"),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+          : undefined,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     clientId: process.env.FIREBASE_CLIENT_ID,
     authUri: "https://accounts.google.com/o/oauth2/auth",
